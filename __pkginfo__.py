c105028b50fad7dd059f8af328eebdc71bdf38b5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-21 22:48:13 +0100

    Bump version: 0.0.0 → 0.0.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 modname = "flake8-dunder-all"
 pypi_name = "flake8-dunder-all"
 import_name = "flake8_dunder_all"
