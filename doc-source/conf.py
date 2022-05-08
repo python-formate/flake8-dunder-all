4c98a8adba468aad96b0632fb9d6fa88d8ff4d48
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-05-08 15:20:40 +0100

    Update docs.

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -77,4 +77,10 @@ def setup(app):
 	app.connect("config-inited", lambda app, config: better_header_layout(config))
 
 
-autosummary_widths_builders = ["latex"]
+needspace_amount = r"5\baselineskip"
+favicons = [{
+		"rel": "icon",
+		"href": "https://python-formate.github.io/assets/formate.ico",
+		"sizes": "48x48",
+		"type": "image/vnd.microsoft.icon"
+		}]
