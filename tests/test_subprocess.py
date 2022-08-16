5f11dca2c03ea0c0a3a73848f1f231d46149ece5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-08-16 10:56:27 +0100

    Add test running flake8 in a subprocess.

---
+++ tests/test_subprocess.py
@@ -0,0 +1,86 @@
+# stdlib
+import os
+import subprocess
+import sys
+
+# 3rd party
+from domdf_python_tools.paths import PathPlus, in_directory
+
+
+def test_subprocess(tmp_pathplus: PathPlus, monkeypatch):
+	monkeypatch.delenv("COV_CORE_SOURCE")
+	monkeypatch.delenv("COV_CORE_CONFIG")
+	monkeypatch.delenv("COV_CORE_DATAFILE")
+
+	(tmp_pathplus / "code.py").write_text("""
+
+def foo():
+	pass
+	""")
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 1
+	assert result.stdout == b"""\
+code.py:1:1: DALL000 Module lacks __all__.
+code.py:2:1: W191 indentation contains tabs
+code.py:2:1: W293 blank line contains whitespace
+code.py:4:1: W191 indentation contains tabs
+code.py:5:1: W191 indentation contains tabs
+code.py:5:1: W293 blank line contains whitespace
+code.py:5:2: W292 no newline at end of file
+"""
+	assert result.stderr == b''
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py", "--select", "DALL000"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 1
+	assert result.stdout == b"code.py:1:1: DALL000 Module lacks __all__.\n"
+	assert result.stderr == b''
+
+	(tmp_pathplus / "tox.ini").write_text("""
+
+[flake8]
+select = DALL000
+	""")
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 1
+	assert result.stdout == b"code.py:1:1: DALL000 Module lacks __all__.\n"
+	assert result.stderr == b''
+
+	tox_ini = tmp_pathplus / "tox.ini"
+	tox_ini.write_text("""
+
+[flake8]
+select = DALL000
+per-file-ignores =
+    code.py: DALL000
+	""")
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 0
+	assert result.stdout == b''
+	assert result.stderr == b''
