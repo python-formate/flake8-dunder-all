da5b0bc9d18021ddbc29cd9c3feb9d66e45b69c0
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-12-13 16:26:02 +0000

    Add subprocess test for # noqa comments

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -81,3 +81,78 @@ per-file-ignores =
 	assert result.returncode == 0
 	assert result.stdout == b''
 	assert result.stderr == b''
+
+
+def test_subprocess_noqa(tmp_pathplus: PathPlus, monkeypatch):
+	monkeypatch.delenv("COV_CORE_SOURCE", raising=False)
+	monkeypatch.delenv("COV_CORE_CONFIG", raising=False)
+	monkeypatch.delenv("COV_CORE_DATAFILE", raising=False)
+	monkeypatch.setenv("PYTHONWARNINGS", "ignore")
+
+	(tmp_pathplus / "code.py").write_text("# noqa: DALL000\n\n\t\ndef foo():\n\tpass\n\t")
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 1
+	assert result.stdout == b"""\
+code.py:3:1: W191 indentation contains tabs
+code.py:3:1: W293 blank line contains whitespace
+code.py:5:1: W191 indentation contains tabs
+code.py:6:1: W191 indentation contains tabs
+code.py:6:1: W293 blank line contains whitespace
+code.py:6:2: W292 no newline at end of file
+"""
+	assert result.stderr == b''
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py", "--select", "DALL000"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 0
+	assert result.stdout == b''
+	assert result.stderr == b''
+
+	(tmp_pathplus / "tox.ini").write_text("""
+
+[flake8]
+select = DALL000
+	""")
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 0
+	assert result.stdout == b''
+	assert result.stderr == b''
+
+	tox_ini = tmp_pathplus / "tox.ini"
+	tox_ini.write_text("""
+
+[flake8]
+select = DALL000
+per-file-ignores =
+    code.py: DALL000
+	""")
+
+	with in_directory(tmp_pathplus):
+		result = subprocess.run(
+				[sys.executable, "-m", "flake8", "code.py"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				)
+
+	assert result.returncode == 0
+	assert result.stdout == b''
+	assert result.stderr == b''
